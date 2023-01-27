fa3e90115d9bab3defa1dfe4643b4b9eade99523
yukkop <hectic.yukkop@gmail.com>
2023-01-27 05:44:47 +0100

    feat: region page to conf

--- bin/pages/region/choice-region.page.dart
+++ bin/pages/region/choice-region.page.dart
@@ -1,22 +1,26 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
+import 'package:vpn_telegram_bot/data/layout.enum.dart';
 import 'package:vpn_telegram_bot/page-giga-mega-trash/button.hectic-tg.dart';
 import 'package:vpn_telegram_bot/page-giga-mega-trash/keyboard.hectic-tg.dart';
 import 'package:vpn_telegram_bot/page-giga-mega-trash/page.hectic-tg.dart';
 import 'package:vpn_telegram_bot/page-giga-mega-trash/text.hectic-tg.dart';
 
 import '../../configurations.dart';
+import '../../variables.dart';
 import '../main.page.dart';
 import 'change-region.action.dart';
 
 final regionChoiceEdit = Page(
-  text: Text.string('''Какой регион желаете?'''),
+  text: Text.string(dialogDataSource.getMessage(
+      'region${dialogDataSource.separator}choice-region', LayoutEnum.ru)),
   renderMethod: Page.edit,
 );
 
 final regionChoiceReplace = Page(
-  text: Text.string('''Какой регион желаете?'''),
+  text: Text.string(dialogDataSource.getMessage(
+      'region${dialogDataSource.separator}choice-region', LayoutEnum.ru)),
   renderMethod: (teleDart, message, user, text, markup) async {
     Page.replase(teleDart, message.chat.id, text, markup, message.message_id);
   },
@@ -31,13 +35,22 @@ void testPeriodChoiceRegionKeyboard() {
     for (var i in responseBody) {
       arr.add([
         Button.openPage(
-            text: (i['regionName'] == 'russia' ? 'Русский' : 'Германия'),
+            text: (i['regionName'] == 'russia'
+                ? 'Русский 🇷🇺'
+                : 'Германия 🇩🇪'),
             key: (i['regionName'] == 'russia'
                 ? changeRegionRussia.getKey()
                 : changeRegionGermany.getKey()))
       ]);
     }
-    arr.add([Button.openPage(text: 'Назад', key: mainMenuEdit.getKey())]);
+    arr.add([
+      Button.openPage(
+          text: dialogDataSource.getButtonText(
+              'region${dialogDataSource.separator}choice-region',
+              'back',
+              LayoutEnum.ru),
+          key: mainMenuEdit.getKey())
+    ]);
 
     return arr;
   });
